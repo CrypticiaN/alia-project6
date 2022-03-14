c815bfc8b9e46147050386d8686919ddd9f19b84
AnaCasasC <cervelloana@gmail.com>
2022-03-14 10:11:48 +0100

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "gatsby";
-import github from "../img/github-icon.svg";
 import logo from "../img/logo.svg";
 
 const Navbar = class extends React.Component {
@@ -64,38 +63,26 @@ const Navbar = class extends React.Component {
           >
             <div className="navbar-start has-text-centered">
               <Link className="navbar-item" to="/about">
-                About
-              </Link>
-              <Link className="navbar-item" to="/products">
-                Products
-              </Link>
-              <Link className="navbar-item" to="/blog">
-                Blog
+                Nosotras
               </Link>
               <Link className="navbar-item" to="/servicios">
-               Servicios
+                Servicios
               </Link>
               <Link className="navbar-item" to="/proyectos">
                 Proyectos
               </Link>
               <Link className="navbar-item" to="/contact">
-                Contact
+                Contacto
+              </Link>
+{/*              <Link className="navbar-item" to="/products">
+                Products
+              </Link>
+              <Link className="navbar-item" to="/blog">
+                Blog
               </Link>
               <Link className="navbar-item" to="/contact/examples">
                 Form Examples
-              </Link>
-            </div>
-            <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
+              </Link>*/}
             </div>
           </div>
         </div>
